f0f966d953378b0c7194651c63d54201783a6f11
Akobir5 <toshtemirovakobir8@gmail.com>
2022-02-14 13:18:33 +0300

    new tech

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let audioTag = document.querySelector('.audioTag')
+let menuBar = document.querySelectorAll('.menuBar')
+let prev = document.querySelector('.fa-backward')
+let play = document.querySelector('.fa-play')
+let next = document.querySelector('.fa-forward')
+let music_duration = document.querySelector('.music_duration')
+let music_time = document.querySelector('.music_time')
+
+let musics = [ 'Adam', 'Billie Eilish & Khalid', 'Bruno Mars', 'Davina Michelle', 'Doston Ergashev', 'Ed Sheeran', `G'aybulla Tursunov`, 'Justin' ]
+
+let music_name = [
+    ``
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
