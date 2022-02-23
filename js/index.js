135211efce4e4449ac4d0999f874cc196f5d7bcb
Akobir5 <https://github.com/Akobir5/flashup-promo>
2022-02-23 07:11:42 +0500

    newww w1

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ let music_photo = [
     `https://img.discogs.com/9gLVVyZ5Tmd2l8WvEScNC1LQyYk=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9742077-1485647103-1247.jpeg.jpg`,
     `https://is5-ssl.mzstatic.com/image/thumb/Music124/v4/ff/54/ef/ff54ef6a-3197-b4e9-12bf-39d5a155b592/cover.jpg/400x400cc.jpg`,
     `https://img.youtube.com/vi/IjNTuBgOkz0/hqdefault.jpg  `,
-    `https://cdn51.zvuk.com/pic?type=release&id=16317172&ext=jpg&size=200x200`,
+    `https://m.media-amazon.com/images/I/81lWGt6TTpS._SS500_.jpg`,
     `https://i.ytimg.com/vi/md65GIQ9Cu8/maxresdefault.jpg`,
     `https://cdns-images.dzcdn.net/images/cover/0f70934b35efe1af954f2ab8ada803ff/500x500.jpg`
 ]
@@ -173,16 +173,19 @@ for(let j = 0; j < musics.length; j++) {
 
     let box = document.createElement('div')
     box.classList.add('homeOfMusic')
+    let numberAndImg = document.createElement('div')
+    box.appendChild(numberAndImg)
+    numberAndImg.classList.add('numberAndImg')
     let number = document.createElement('h4')
     number.classList.add('number')
     number.innerText = numOfMusics[j]
-    box.appendChild(number)
+    numberAndImg.appendChild(number)
     let img = document.createElement('img')
     img.setAttribute('src', music_photo[j])
-    box.appendChild(img)
+    numberAndImg.appendChild(img)
     let textBlock  = document.createElement('div')
     textBlock.classList.add('textBlock')
-    box.appendChild(textBlock)
+    numberAndImg.appendChild(textBlock)
     let name = document.createElement('h3')
     name.innerText = music_name[j]
     textBlock.appendChild(name)
@@ -191,10 +194,13 @@ for(let j = 0; j < musics.length; j++) {
     textBlock.appendChild(actor)
     let control = document.createElement('div')
     control.classList.add('likeControl')
-    control.innerHTML = `
-
-    `
-    
+    let like = document.createElement('img')
+    like.setAttribute('src', `https://wisperwindoxas.github.io/sneaker/img/heart-unliked.svg`)
+    like.classList.add('like')
+    control.appendChild(like)
+    let sounds = document.createElement('img')
+    sounds.classList.add('sounds')
+    control.appendChild(sounds)
     box.appendChild(control)
     menuBar[1].appendChild(box)
 }
@@ -202,19 +208,55 @@ for(let j = 0; j < musics.length; j++) {
 let numLarge = document.querySelectorAll('.number')
 numLarge[9].classList.add('large')
 
-let box_homeOfMusic = document.querySelectorAll('.homeOfMusic')
+let box_homeOfMusic = document.querySelectorAll('.numberAndImg')
 
 box_homeOfMusic.forEach((item, index) => {
     item.addEventListener('click', () => {
         item.classList.toggle('stopping')
         audioTag.setAttribute('src', `./music/${musics[index]}.mp3`)
         playMusic()
+        artistName.innerText = musics[index]
+        musicPhoto.setAttribute('src', music_photo[index])
+        musicNames.innerText = music_name[index]
+        
+        let soundAnim = document.querySelectorAll('.sounds')
+        // soundAnim.forEach(item => {
+        //     item.setAttribute('src', `https://m.media-amazon.com/images/G/01/digital/music/player/web/sixteen_frame_equalizer_accent.gif`)
+        // })
+
 
         if(item.classList.contains('stopping')) {
             playMusic()
+            soundAnim[index].setAttribute('src', `https://m.media-amazon.com/images/G/01/digital/music/player/web/sixteen_frame_equalizer_accent.gif`)
+    
         } else {
            stopMusic()
+           soundAnim[index].at
         }
     })
 })
+
+
+let unlike = document.querySelectorAll('.like')
+
+unlike.forEach(item => {
+    item.addEventListener('click', () => {
+            item.classList.toggle("redPlus")
+    
+            if (item.classList.contains('redPlus')) {
+                item.setAttribute('src', 'https://wisperwindoxas.github.io/sneaker/img/heart-liked.svg')
+            }else {
+                item.setAttribute('src', 'https://wisperwindoxas.github.io/sneaker/img/heart-unliked.svg')
+            }
+    })
+})
+
+let likeControl = document.querySelectorAll('.likeControl')
+
+if(songNum.valueOf() === box_homeOfMusic.valueOf()) {
+    let sounds = document.createElement('img')
+    sounds.setAttribute('src', `https://maxst.icons8.com/vue-static/landings/animated-icons/icons/sound/sound.json`)
+    box_homeOfMusic.appendChild(sounds)
+    console.log(sounds);
+}
 
